2a69dbd0e0de7ec2343ae39a65a8cb37ad718835
krina1902 <krinaantala33@gmail.com>
2023-12-30 19:15:41 +0530

    py

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name='index'),
+    path('about',views.about,name='about'),
+    path('class',views.classes,name='class'),
+    path('team',views.team,name='team'),
+    path('gallery',views.gallery,name='gallery'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
 	return render(request,'index.html')
+
+def about(request):
+	return render(request,'about.html')
+
+def classes(request):
+	return render(request,'class.html')
+
+def team(request):
+	return render(request,'team.html')
+
+def gallery(request):
+	return render(request,'gallery.html')
